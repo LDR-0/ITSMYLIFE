5190ec878944b252011cc30e870b70323736f278
LDR-0 <dlezhayko@gmail.com>
2024-04-04 13:25:39 +0200

    add

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-// @ts-check
-
 import { mkdir, mkfile } from '@hexlet/immutable-fs-trees';
 
 const generate = () => {
--- index.test.js
+++ index.test.js
@@ -1,5 +1,3 @@
-// @ts-check
-
 import generate from '../index.js';
 
 test('generate', () => {
